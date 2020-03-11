cd5564ca724d1ac848d0d831f076ab41b1201c00
Denis Gribanov <d.gribanov@dev.untill.com>
2020-03-11 16:37:16 +0300

    continue

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ fi
 echo "Building cder..."
 go build
 echo "Logging in to dockerhub"
-docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
 echo "Creating docker images..."
-docker build -t "${DOCKER_USERNAME}"/cdernode:v"$VER" -f ./node/Dockerfile .
-docker build -t "${DOCKER_USERNAME}"/cder:v"$VER" -f ./go/Dockerfile .
+docker build -t "${DOCKER_USERNAME}"/cdernode:v"${VER}" -f ./node/Dockerfile .
+docker build -t "${DOCKER_USERNAME}"/cder:v"${VER}" -f ./go/Dockerfile .
 echo "Pushing images..."
 #docker push "${DOCKER_USERNAME}"/cdernode:v"$VER"
 #docker push "${DOCKER_USERNAME}"/cder:v"$VER"
