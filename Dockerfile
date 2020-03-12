3ba690ccbcaa959764b33ba405175b66a2c0ffa2
Denis Gribanov <d.gribanov@dev.untill.com>
2020-03-12 16:59:04 +0300

    continue

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM golang:1.11.9
 
 RUN mkdir /cder
 COPY directcd /cder/cder
+COPY prep.sh /cder/prep.sh
 RUN chmod 777 /cder/cder
 
 ENTRYPOINT ["/cder/cder"]
---
+++ prep.sh
@@ -0,0 +1,2 @@
+git config --global url."https://builder-untill:3508354fc58a2c1ada2c5f6ef2b0bb864695a32b@github.com/untillpro".insteadOf "https://github.com/untillpro"
+
